d80b9b6c751d1b4b95e4b937b66313cf7fa5b763
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-07 21:44:58 -0300

    Aula gets e puts

--- aprendendo.rb
+++ aprendendo.rb
@@ -5,3 +5,9 @@ print "Aprendendo Ruby!\n"
 # não é necessário usar ()
 puts ("Testando " + testando)
 puts ("Meu número favorito é " + num.to_s())
+puts "Oi, qual o seu nome?"
+nome = gets()
+puts ("Prazer em conhecê-lo " + nome)
+puts "Quantos anos você tem?"
+idade = gets.chomp()
+puts ("Você tem " + idade + " anos")
